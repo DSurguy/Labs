ef2b8fb759f0a1bef4dc661092ac6432d85487b1
Derek Surguy <Surguy.Derek@gmail.com>
2014-07-11 14:44:21 -0500

    Worked on animated dialog
    Added gsap
    Updated gitignore

--- AnimatedStuff/modalPlugin.js
+++ AnimatedStuff/modalPlugin.js
@@ -1,58 +1,87 @@
 $.fn.modal = function(headerHtml, bodyHtml, options){
 
-	/*options
 	var options = {
-		css: {
-			key: value,
-			...
+		width: 300,
+		height: 460,
+		theme: "a",
+		beforeLoad: function(){},
+		afterLoad: function (modalBody){
+			
 		},
-		class: 'testModal',
-
+		actions: [{
+			icon: "test.png",
+			label: "Test Action",
+			color: "green",
+			action: function(){}
+		}]
 	}
-	*/
 
-	var modalContain = $("<div class=\""+Date.now()+"-modalBox\"></div>"),
-		newModal = $("<div class=\""+Date.now()+"-modal\"></div>");
+	var modalContain = $("<div class=\""+Date.now()+"-m modalBox theme-"+options.theme+"\"></div>"),
+		newModal = $("<div class=\""+Date.now()+"-m modal theme-"+options.theme+"\"></div>");
 
 	modalContain.append(newModal);
 
 	//add the header
-	newModal.append("<div class=\"header\"></div>");
+	newModal.append("<div class=\"header\"></div>")
+
 	//add the body
 	newModal.append("<div class=\"body\"></div>");
 
-	//apply default css
+	//add the actions
+	newModal.append("<div class=\"actions\"></div>");
+
+	//add the header html
+	newModal.find(".header").append(headerHtml);
+
+	//add the body html and controls
+	newModal.find(".body").append(bodyHtml);
+
+	//add the action buttons
+	var actions = newModal.find(".actions");
+
+	for( var i=0; i<options.actions.length; i++ ){
+		var thisAction = options.actions[i];
+		actions.append("<div class=\"action\" style=\"background-color: "+thisAction.color+"\"><img src=\"path/"+thisAction.icon+"\" />"+thisAction.label+"</div>");
+	}
+
+	//set some display props
+
 	modalContain
-		.css("background","rgba(255,255,255,0.8)")
+		.css("width", "100%")
+		.css("height","100%")
 		.css("position","absolute")
-		.css("top","0")
-		.css("left","0")
-		.css("width","100%")
-		.css("height","100%");
+		.css("top", "0")
+		.css("left", "0")
+		.css("background","rgba(0,0,0,0.7)");
 
 	newModal
-		.css("display", "none")
-		.css("position", "absolute")
+		.css("width", options.width+"px")
+		.css("height", options.height+"px")
+		.css("position","absolute")
+		.css("background-color","#fff");
+
+	//set some postioning props
+	newModal
 		.css("top","50%")
 		.css("left","50%")
-		.css("transform","translate(-50%)");
+		.css("margin-left","-"+options.width/2+"px")
+		.css("margin-top", "-"+options.height/2+"px");
 
-	newModal.find(".header")
-		.css("padding","10px")
-		.css("font-size", "1.8rem")
-		//add the header html
-		.append(headerHtml);
+	//prep the animation
+	TweenLite.set(newModal, {scaleX:0, scaleY:0});
+	TweenLite.set(modalContain, {background: "rgba(0,0,0,0)"})
 
-	newModal.find(".body")
-		.css("padding","10px")
-		.css("font-size","1rem")
-		//add the body html
-		.append(bodyHtml);
 
 	//add the modal to the DOM
 	this.prepend(modalContain);
 
-	//animate the display of the modal
-	newModal.css("display","block").addClass("anim-dsModalPopIn");
+	//animate the dispay
+	var tLine = new TimelineLite();
+
+	tLine.to(modalContain, 0.10, {background: "rgba(0,0,0,0.8)"});
+	tLine.to(newModal, 0.12, {scaleX: 1.1, scaleY: 1.1});
+	tLine.to(newModal, 0.06, {scaleX: 1.0, scaleY: 1.0});
+
+	tLine.play();
 
 };
